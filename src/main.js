fff21fd426217025e7aa49f7991585d55058ce98
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-16 16:09:06 -0700

    clicking only displays age if a number was entered for age

--- src/main.js
+++ src/main.js
@@ -4,43 +4,57 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Planet from './planet.js';
 
+
+
 $(document).ready(function() {
   let planet;
   let age;
+
+  function checkNaN(age){
+    if(Number.isNaN(age)){
+      $("#display-planet-age").hide();
+      $("#reminder").show();
+      console.log("you are here");
+    } else {
+      $("#display-planet-age").show();
+      $("#reminder").hide();
+    }
+  }
+
   $("#mercury-age").click(function(event){
     event.preventDefault();
     age=parseInt($("#age").val());
+    checkNaN(age);
     planet=new Planet(age);
     $(".planet-name").text("Mercury");
     $("#life-expectancy").text(planet.getLifeExpectancy("Mercury"));
     $("#planet-age").text(planet.getMercuryAge());
-    $("#display-planet-age").show();
   });
   $("#venus-age").click(function(event){
     event.preventDefault();
     age=parseInt($("#age").val());
+    checkNaN(age);
     planet=new Planet(age);
     $(".planet-name").text("Venus");
     $("#life-expectancy").text(planet.getLifeExpectancy("Venus"));
     $("#planet-age").text(planet.getVenusAge());
-    $("#display-planet-age").show();
   });
   $("#mars-age").click(function(event){
     event.preventDefault();
     age=parseInt($("#age").val());
+    checkNaN(age);
     planet=new Planet(age);
     $(".planet-name").text("Mars");
     $("#life-expectancy").text(planet.getLifeExpectancy("Mars"));
     $("#planet-age").text(planet.getMarsAge());
-    $("#display-planet-age").show();
   });
   $("#jupiter-age").click(function(event){
     event.preventDefault();
     age=parseInt($("#age").val());
+    checkNaN(age);
     planet=new Planet(age);
     $(".planet-name").text("Jupiter");
     $("#life-expectancy").text(planet.getLifeExpectancy("Jupiter"));
     $("#planet-age").text(planet.getJupiterAge());
-    $("#display-planet-age").show();
   });
 });
