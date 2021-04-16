20f14173b7df2bf6cc197f700cdb80730edd9ee1
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-16 14:59:42 -0700

    add UI logic - clicking buttons fills in answers

--- src/main.js
+++ src/main.js
@@ -2,8 +2,42 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import Example from './example.js';
+import Planet from './planet.js';
 
 $(document).ready(function() {
-  
+  let planet;
+  let age;
+  alert("JQuery connected");
+  $("#mercury-age").click(function(event){
+    event.preventDefault();
+    age=parseInt($("#age").val());
+    planet=new Planet(age);
+    $(".planet-name").text("Mercury");
+    $("#life-expectancy").text(planet.getLifeExpectancy("Mercury"));
+    $("#planet-age").text(planet.getMercuryAge());
+  });
+  $("#venus-age").click(function(event){
+    event.preventDefault();
+    age=parseInt($("#age").val());
+    planet=new Planet(age);
+    $(".planet-name").text("Venus");
+    $("#life-expectancy").text(planet.getLifeExpectancy("Venus"));
+    $("#planet-age").text(planet.getVenusAge());
+  });
+  $("#mars-age").click(function(event){
+    event.preventDefault();
+    age=parseInt($("#age").val());
+    planet=new Planet(age);
+    $(".planet-name").text("Mars");
+    $("#life-expectancy").text(planet.getLifeExpectancy("Mars"));
+    $("#planet-age").text(planet.getMarsAge());
+  });
+  $("#jupiter-age").click(function(event){
+    event.preventDefault();
+    age=parseInt($("#age").val());
+    planet=new Planet(age);
+    $(".planet-name").text("Jupiter");
+    $("#life-expectancy").text(planet.getLifeExpectancy("Jupiter"));
+    $("#planet-age").text(planet.getJupiterAge());
+  });
 });
