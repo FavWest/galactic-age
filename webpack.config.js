5165f33d211d9d0c32f825b811918807c9530a30
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-16 08:46:10 -0700

    replace template name with project name

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Example',
+      title: 'Galactic Age',
       template: './src/index.html',
       inject: 'body'
     })
